51e486d6341c6af52fc1a7871ba6c3bc0a284f25
NicolasHasbun <Ni.Hasbun.Molina@gmail.com>
2023-07-18 19:32:13 -0400

    commit de prueba

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ const productos = [
     {
         id: "poster-01",
         titulo: "Poster 01",
-        imagen: "../assets/img/poster/poster1.JPG",
+        imagen: "../assets/img/poster/poster1.jpg",
         categoria: {
             nombre: "Posters",
             id: "posters"
